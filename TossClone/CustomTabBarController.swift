316f081a420fa0210f09d51994bc671e95104ad5
yeongwoo_Cho <jyy0223@naver.com>
2022-07-04 16:11:40 +0900

    [Add] CustomTabBarController and HomeViewController file

---
+++ TossClone/CustomTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarController.swift
+//  TossClone
+//
+//  Created by yeongwoocho on 2022/07/04.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
---
+++ TossClone/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  TossClone
+//
+//  Created by yeongwoocho on 2022/07/04.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
